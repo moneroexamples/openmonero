90cb73f711fd74ce1a5ef7314a4f8b56a759740c
moneroexamples <moneroexamples@tuta.io>
2016-12-08 08:27:36 +0800

    ssl enabled

--- README.md
+++ README.md
@@ -3,10 +3,13 @@
 Example of using [restbed](https://github.com/Corvusoft/restbed/) to provide Monero related JSON REST service.
 
 # Scrap notes
+
+## Gererate your won ssl certificate 
+ 
 Setting up https and ssl certificates in restbed
  - https://github.com/Corvusoft/restbed/blob/34187502642144ab9f749ab40f5cdbd8cb17a54a/example/https_service/source/example.cpp
 
-From the link above:
+Based on the link above:
  
 ```bash
 # Create Certificate
@@ -14,13 +17,23 @@ cd /tmp
 openssl genrsa -out server.key 1024
 openssl req -new -key server.key -out server.csr
 openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
-openssl dhparam -out dh768.pem 768
+openssl dhparam -out dh2048.pem 2048
 ```
+
+## Enable it in Firefox or Chrome
+
+Firefox and chrome will not work with that certificate as they cant verify it. 
+To overcome this for development purposes on localhost, just open new tab in the browser used
+and go to any link from the service, e.g., `https://localhost:1984/login`. Once you do this,
+you should get warring about unsecured or un verified certificate. Then you just add it manually
+as exception.
  
-Example of curl https request
+## Test connection using curl
+
+Example of curl https request to the service
 
 ```bash
-curl -k -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "id": "test", "method": "notifyServer","params": {}}' https://localhost:1984/resource
+curl -k -X POST -d '{"withCredentials":true,"address":"41pJD13rU5r3KZsxzS65tL9zLMpZZCer8aWSi7wj8Xm99BAgXthcj2wgazxdTX9auFAmp3czfJUGH2S3UJfLwDWXUxc3ooC","view_key":"06d1f0f0fd766c75b52b9c597592d06f4bca5cd6dcd3e9bf1859bc78d0d5f80e","create_account":true}' https://localhost:1984/login
 ```
 
 ## Other examples
--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ make_headers(const multimap<string, string>& extra_headers = multimap<string, st
 {
     multimap<string, string> headers {
         {"Date", get_current_time()},
-        {"Access-Control-Allow-Origin",      "http://127.0.0.1"},
+        {"Access-Control-Allow-Origin",      "https://localhost"},
         {"access-control-allow-headers",     "*, DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Set-Cookie"},
         {"access-control-max-age",           "86400, 1728000"},
         {"access-control-allow-methods",     "GET, POST, OPTIONS"},
@@ -59,101 +59,6 @@ generic_options_handler( const shared_ptr< Session > session )
         cout << "requriest options_method_handler" << endl;
 
         session->close( OK, string{}, make_headers());
-    } );
-}
-
-
-
-
-void
-post_login_handler( const shared_ptr< Session > session )
-{
-    const auto request = session->get_request( );
-
-    size_t content_length = request->get_header( "Content-Length", 0);
-
-    cout << "post_method_handler" << endl;
-
-    session->fetch(content_length, [ ]( const shared_ptr< Session > session, const Bytes & body )
-    {
-        string _body(reinterpret_cast<const char *>(body.data()), body.size());
-
-        cout << _body << endl;
-
-        json j_response {{"new_address", true}};
-
-        string response_body = j_response.dump();
-
-        auto response_headers = make_headers({{ "Content-Length", to_string(response_body.size())}});
-
-        session->close( OK, response_body, response_headers);
-    } );
-}
-
-void
-post_get_address_txs_handler( const shared_ptr< Session > session )
-{
-    const auto request = session->get_request( );
-
-    size_t content_length = request->get_header( "Content-Length", 0);
-
-    cout << "post_get_address_txs_handler" << endl;
-
-    session->fetch(content_length, [ ]( const shared_ptr< Session > session, const Bytes & body )
-    {
-        string _body(reinterpret_cast<const char *>(body.data()), body.size());
-
-        cout << _body << endl;
-
-        json j_response {
-                { "total_received", "0"},
-                { "scanned_height", 2012455},
-                { "scanned_block_height", 1195848},
-                { "start_height", 2012455},
-                { "transaction_height", 2012455},
-                { "blockchain_height", 1195848}
-        };
-
-        string response_body = j_response.dump();
-
-        auto response_headers = make_headers({{ "Content-Length", to_string(response_body.size())}});
-
-        session->close( OK, response_body, response_headers);
-    } );
-}
-
-void
-post_get_address_info_handler( const shared_ptr< Session > session )
-{
-    const auto request = session->get_request( );
-
-    size_t content_length = request->get_header( "Content-Length", 0);
-
-    cout << "post_get_address_info_handler" << endl;
-
-    session->fetch(content_length, [ ]( const shared_ptr< Session > session, const Bytes & body )
-    {
-        string _body(reinterpret_cast<const char *>(body.data()), body.size());
-
-        cout << _body << endl;
-
-        json j_response  {
-                {"locked_funds", "0"},
-                {"total_received", "0"},
-                {"total_sent", "0"},
-                {"scanned_height", 2012470},
-                {"scanned_block_height", 1195850},
-                {"start_height", 2012470},
-                {"transaction_height", 2012470},
-                {"blockchain_height", 1195850},
-                {"spent_outputs", nullptr}
-        };
-
-        string response_body = j_response.dump();
-
-        auto response_headers = make_headers({{ "Content-Length", to_string(response_body.size())}});
-
-        session->close( OK, response_body, response_headers);
     } );
 }
 
@@ -261,26 +166,30 @@ main()
 
     auto login            = make_resource("/login"           , handel_(request_login));
     auto get_address_txs  = make_resource("/get_address_txs" , handel_(request_get_address_txs));
-    auto get_address_info = make_resource("/get_address_info", handel_(request_get_address_txs));;
+    auto get_address_info = make_resource("/get_address_info", handel_(request_get_address_txs));
 
-//
-//    auto ssl_settings = make_shared< SSLSettings >( );
-//    ssl_settings->set_http_disabled( true );
-//    ssl_settings->set_port( 1984 );
-//    ssl_settings->set_private_key( Uri( "file:///tmp/server.key" ) );
-//    ssl_settings->set_certificate( Uri( "file:///tmp/server.crt" ) );
-//    ssl_settings->set_temporary_diffie_hellman( Uri( "file:///tmp/dh768.pem" ) );
-//
-//
-//    auto settings = make_shared< Settings >( );
-//    settings->set_ssl_settings( ssl_settings );
-
+    bool use_ssl {true};
 
     auto settings = make_shared< Settings >( );
-    settings->set_port( 1984 );
-    settings->set_default_header( "Connection", "close" );
 
-    cout << "Start the service at https://127.0.0.1:1984" << endl;
+    if (use_ssl)
+    {
+        auto ssl_settings = make_shared< SSLSettings >( );
+
+        ssl_settings->set_http_disabled( true );
+        ssl_settings->set_port(1984);
+        ssl_settings->set_private_key( Uri( "file:///tmp/mwo.key" ) );
+        ssl_settings->set_certificate( Uri( "file:///tmp/mwo.crt" ) );
+        ssl_settings->set_temporary_diffie_hellman( Uri( "file:///tmp/dh2048.pem" ) );
+
+        settings->set_ssl_settings( ssl_settings );
+    }
+
+    //settings->set_default_header("Connection", "close" );
+    //settings->set_port(1984);
+
+
+    cout << "Start the service at https://localhost:1984" << endl;
 
     Service service;
     service.publish(login);
