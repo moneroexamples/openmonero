6a0712b4483e35b6909589c0ae222ce114ac656a
moneroexamples <moneroexamples@tuta.io>
2018-07-13 14:33:04 +0800

    MicroCore cleaned

--- src/MicroCore.h
+++ src/MicroCore.h
@@ -46,49 +46,15 @@ public:
     get_core();
 
     bool
-    get_block_by_height(const uint64_t& height, block& blk);
-
-    bool
     get_block_from_height(const uint64_t& height, block& blk);
 
     bool
     get_tx(const crypto::hash& tx_hash, transaction& tx);
 
-    bool
-    get_tx(const string& tx_hash, transaction& tx);
-
-    bool
-    find_output_in_tx(const transaction& tx,
-                      const public_key& output_pubkey,
-                      tx_out& out,
-                      size_t& output_index);
-
-    bool
-    get_tx_hash_from_output_pubkey(const public_key& output_pubkey,
-                                   const uint64_t& block_height,
-                                   crypto::hash& tx_hash,
-                                   transaction& tx_found);
-
-    uint64_t
-    get_blk_timestamp(uint64_t blk_height);
-
-    string
-    get_blkchain_path();
-
-
-
-    bool
-    get_random_outs_for_amounts(const uint64_t& amount,
-                                const uint64_t& no_of_outputs,
-                                vector<pair<uint64_t, public_key>>& found_outputs);
-
     hw::device* const
     get_device() const;
 
     virtual ~MicroCore();
-
-
-
 
 };
 
