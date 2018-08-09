beccdceeb66325c3b8a66a89d9ffb213f4d118f8
0xA01 <cryptoape@icloud.com>
2018-08-09 19:18:52 +0300

    Flake REST v0.0.1

--- html/js/directives/transacation.js
+++
@@ -1,7 +0,0 @@
-thinwalletDirectives.directive('transaction', function () {
-    return {
-        restrict: 'E',
-        templateUrl:"/js/directives/transaction.html",
-        replace: true,
-    };
-});
