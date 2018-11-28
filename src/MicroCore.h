eb34693b378fdca475b05d81ef8b2f49070ef751
moneroexamples <moneroexamples@tuta.io>
2018-11-28 14:53:45 +0800

    fix: get_output_key requires epee::span

--- src/MicroCore.h
+++ src/MicroCore.h
@@ -72,7 +72,8 @@ public:
                    vector<cryptonote::output_data_t>& outputs)
     {
         core_storage.get_db()
-                .get_output_key(amount, absolute_offsets, outputs);
+                .get_output_key(epee::span<const uint64_t>(&amount, 1),
+                                absolute_offsets, outputs);
     }
 
     virtual output_data_t
