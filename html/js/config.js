333dbe9c594e9216efced4caed7a9db3cc2f79b5
moneroexamples <moneroexamples@tuta.io>
2019-01-27 08:31:27 +0800

    Change nettype to stagenet

--- html/js/config.js
+++ html/js/config.js
@@ -3,7 +3,7 @@ var config = {
     mainnetExplorerUrl: "https://xmrchain.com/",
     testnetExplorerUrl: "https://testnet.xmrchain.com/",
     stagenetExplorerUrl: "http://139.162.60.17:8082/",
-    nettype: 0, /* 0 - MAINNET, 1 - TESTNET, 2 - STAGENET */
+    nettype: 2, /* 0 - MAINNET, 1 - TESTNET, 2 - STAGENET */
     coinUnitPlaces: 12,
     txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
     txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
