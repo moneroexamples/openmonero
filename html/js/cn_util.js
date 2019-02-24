8630bbbef48bbcde2b8bf76305dd2e5116d2499c
moneroexamples <moneroexamples@tuta.io>
2019-02-24 12:18:05 +0800

    correct create_address in js

--- html/js/cn_util.js
+++ html/js/cn_util.js
@@ -442,7 +442,14 @@ var cnUtil = (function(initConfig) {
             first = seed; //only input reduced seeds or this will not give you the result you want
         }
         keys.spend = this.generate_keys(first);
-        var second = this.cn_fast_hash(first);
+
+        var second;
+        if (seed.length !== 64) {
+            second = this.cn_fast_hash(first);
+        } else {
+            second = this.cn_fast_hash(keys.spend.sec);
+        }
+
         keys.view = this.generate_keys(second);
         keys.public_addr = this.pubkeys_to_string(keys.spend.pub, keys.view.pub);
         return keys;
