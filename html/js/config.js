2f2451c93f6dae3ef0336591a403e36e45bf58c5
sebseb7 <sebgreenbus@gmail.com>
2019-04-27 17:00:12 +0200

    config file update

--- html/js/config.js
+++ html/js/config.js
@@ -1,25 +1,25 @@
 var config = {
-    apiUrl: "http://127.0.0.1:1984/",
-    mainnetExplorerUrl: "https://xmrchain.com/",
-    testnetExplorerUrl: "https://testnet.xmrchain.com/",
-    stagenetExplorerUrl: "http://139.162.60.17:8082/",
-    nettype: 2, /* 0 - MAINNET, 1 - TESTNET, 2 - STAGENET */
+    apiUrl: "/api/",
+    mainnetExplorerUrl: "https://swap.coinscope.cc/",
+    testnetExplorerUrl: "https://swaptest.coinscope.cc/",
+    stagenetExplorerUrl: "http://swapstage.coinscope.cc/",
+    nettype: 0, /* 0 - MAINNET, 1 - TESTNET, 2 - STAGENET */
     coinUnitPlaces: 12,
     txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
     txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-    coinSymbol: 'XMR',
-    openAliasPrefix: "xmr",
-    coinName: 'Monero',
-    coinUriPrefix: 'monero:',
-    addressPrefix: 18,
-    integratedAddressPrefix: 19,
-    subAddressPrefix: 42,
-    addressPrefixTestnet: 53,
-    integratedAddressPrefixTestnet: 54,
-    subAddressPrefixTestnet: 63,
-    addressPrefixStagenet: 24,
-    integratedAddressPrefixStagenet: 25,
-    subAddressPrefixStagenet: 36,
+    coinSymbol: 'XWP',
+    openAliasPrefix: "xwp",
+    coinName: 'Swap',
+    coinUriPrefix: 'swap:',
+    addressPrefix: 10343,
+    integratedAddressPrefix: 13671,
+    subAddressPrefix: 11368,
+    addressPrefixTestnet: 0x5b1d,
+    integratedAddressPrefixTestnet: 0x519e,
+    subAddressPrefixTestnet: 0x641c,
+    addressPrefixStagenet: 10343,
+    integratedAddressPrefixStagenet: 13671,
+    subAddressPrefixStagenet: 11368,
     feePerKB: new JSBigInt('2000000000'),//20^10 - not used anymore, as fee is dynamic.
     dustThreshold: new JSBigInt('1000000000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
     txChargeRatio: 0.5,
@@ -28,6 +28,6 @@ var config = {
     idleTimeout: 30,
     idleWarningDuration: 20,
     maxBlockNumber: 500000000,
-    avgBlockTime: 120,
+    avgBlockTime: 15,
     debugMode: false
 };
